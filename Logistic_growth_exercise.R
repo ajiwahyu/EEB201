9b715f083f755a19518d20e0b4241ca5841b7277
ajiwahyu <ajiwahyu@ucla.edu>
2015-09-16 16:03:53 -0700

    Logistic exercise

---
+++ Logistic_growth_exercise.R
@@ -0,0 +1,12 @@
+No <- 100
+R <- 1.05
+maxtime <- 10
+K <- 0.9
+
+
+NN<- matrix(NA, nrow=1, ncol=maxtime+1)
+NN[1]=100
+for (tt in 1:10) {
+  NN[tt+1]= NN[tt]*    (1+   R*    (1-(NN[tt]/K)))}
+NN
+plot(1:(maxtime+1), NN, xlab="time", ylab="N", col="blue")
