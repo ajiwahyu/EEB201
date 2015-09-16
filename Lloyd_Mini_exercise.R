5a150e65553a7a532a30c5e2e7c432e40dbd074e
ajiwahyu <ajiwahyu@ucla.edu>
2015-09-16 15:41:55 -0700

    mini exercise

---
+++ Lloyd_Mini_exercise.R
@@ -0,0 +1,10 @@
+No <- 100
+R <- 0.7
+maxtime <- 10
+
+NN<- matrix(NA, nrow=1, ncol=maxtime+1)
+NN[1]=100
+for (tt in 1:10) {
+  NN[tt+1]= NN[tt]*R}
+NN
+plot(1:(maxtime+1), NN, xlab="time", ylab="N", col="blue")
