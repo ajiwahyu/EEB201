d9930c086336a6737c3b24a47f627185f3c00873
ajiwahyu <ajiwahyu@ucla.edu>
2015-09-16 15:41:37 -0700

    exercise

--- Lloyd_exercise.R
+++ Lloyd_exercise.R
@@ -1,10 +1,14 @@
-No <- 100
-R <- 1.05
-maxtime <- 10
+geomFunction <- function (No, R, maxtime) {
+  NN<- matrix(NA, nrow=1, ncol=maxtime+1)
+  NN[1]=100
+  for (tt in 1:10) {
+    NN[tt+1]= NN[tt]*R
+  }
+  plot(1:(maxtime+1), NN, xlab="time", ylab="N", col="blue")
+  NN
+  }
 
-NN<- matrix(NA, nrow=1, ncol=maxtime+1)
-NN[1]=100
-for (tt in 1:10) {
-NN[tt+1]= NN[tt]*R}
-NN
-plot(1:(maxtime+1), NN, xlab="time", ylab="N", col="blue")
+geomFunction(10, 0.95, 10)
+
+
+
